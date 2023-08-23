bf72375affd766e1d485f7b5ace4fe2585e68f44
Santiago Uria <uriasantiago.a@gmail.com>
2023-08-23 18:27:20 -0300

    fixes invalid position on neighbour lights, adds loop in App to play

--- src/controller/App.java
+++ src/controller/App.java
@@ -8,8 +8,16 @@ public class App {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Welcome! Which board size would you like to play?");
 		int boardSize = scan.nextInt();
-		Board board = new Board(boardSize);
+
 		
-		System.out.println(board.toString());
+		Board board = new Board(boardSize);
+		while(!board.isCompleted()) {
+			System.out.println(board.toString());
+			System.out.println("Enter the row number");
+			int rowTarget = scan.nextInt();
+			System.out.println("Enter the column number");
+			int colTarget = scan.nextInt();
+			board.switchLightValue(rowTarget, colTarget);
+		}
 	}
 }
--- src/controller/Board.java
+++ src/controller/Board.java
@@ -4,32 +4,74 @@ import java.util.Random;
 
 public class Board {
 	private Light[][] lights;
+	private int tries;
 
 	Board(int size) {
 		lights = new Light[size][size];
+		tries = 0;
 		Random random = new Random();
-		for (int i = 0; i < lights.length; i++) {
-			for (int j = 0; j < lights.length; j++) {
+		for (int i = 0; i < lights.length; i++)
+			for (int j = 0; j < lights.length; j++)
 				lights[i][j] = new Light(random.nextBoolean());
-			}
-		}
-	}
-
-	public boolean getLightValue(int row, int col) {
-		return lights[row][col].getState();
 	}
 
 	public void switchLightValue(int row, int col) {
-		try {
-			lights[row][col].hitSwitch();
-			lights[row - 1][col].hitSwitch();
-			lights[row + 1][col].hitSwitch();
-			lights[row][col - 1].hitSwitch();
-			lights[row][col + 1].hitSwitch();
-		} catch (Exception e) {
-
+		if (validPosition(row, col)) {
+			getLight(row, col).hitSwitch();
+			if (col - 1 >= 0)
+				getLightLeft(row, col).hitSwitch();
+			if (col + 1 < lights.length)
+				getLightRight(row, col).hitSwitch();
+			if (row - 1 >= 0)
+				getLightUp(row, col).hitSwitch();
+			if (row + 1 < lights.length)
+				getLightDown(row, col).hitSwitch();
+			tries++;
+		} else {
+			System.out.println(":( Thats not a valid position. Try again!");
 		}
 
+	}
+
+	public String toString() {
+		StringBuilder board = new StringBuilder();
+		board.append(getDashLine());
+		for (Light[] lightRow : lights)
+			board.append(rowToString(lightRow));
+		board.append(getDashLine());
+		return board.toString();
+	}
+
+	public boolean isCompleted() {
+		for (Light[] lightRow : lights)
+			for (Light light : lightRow)
+				if (light.getState())
+					return false;
+		return true;
+	}
+
+	private boolean validPosition(int row, int col) {
+		return row >= 0 && row < lights.length && col >= 0 && col < lights.length;
+	}
+
+	private Light getLight(int row, int col) {
+		return lights[row][col];
+	}
+
+	private Light getLightLeft(int row, int col) {
+		return lights[row][col - 1];
+	}
+
+	private Light getLightRight(int row, int col) {
+		return lights[row][col + 1];
+	}
+
+	private Light getLightUp(int row, int col) {
+		return lights[row - 1][col];
+	}
+
+	private Light getLightDown(int row, int col) {
+		return lights[row + 1][col];
 	}
 
 	private String rowToString(Light[] lightRow) {
@@ -47,21 +89,10 @@ public class Board {
 
 	private String getDashLine() {
 		StringBuilder dashLine = new StringBuilder("---");
-		for (int i = 0; i < lights.length * 2; i++) {
+		for (int i = 0; i < lights.length * 2; i++)
 			dashLine.append("-");
-		}
 		dashLine.append("\n");
 		return dashLine.toString();
-	}
-
-	public String toString() {
-		StringBuilder board = new StringBuilder();
-		board.append(getDashLine());
-		for (Light[] lightRow : lights) {
-			board.append(rowToString(lightRow));
-		}
-		board.append(getDashLine());
-		return board.toString();
 	}
 
 }
