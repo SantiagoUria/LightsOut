c0b7b3bc3ae045975b964d73ecbbea85233ad98b
Santiago Uria <uriasantiago.a@gmail.com>
2023-08-23 13:48:34 -0300

    adds Light class

---
+++ src/controller/Light.java
@@ -0,0 +1,22 @@
+package controller;
+
+public class Light {
+
+	private boolean isOn;
+
+	Light(boolean isOn) {
+		this.isOn = isOn;
+	}
+
+	protected boolean getState() {
+		return isOn;
+	}
+
+	protected void setState(boolean isOn) {
+		this.isOn = isOn;
+	}
+
+	protected void hitSwitch() {
+		isOn = !isOn;
+	}
+}
