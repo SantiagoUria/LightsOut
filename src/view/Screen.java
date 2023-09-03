166c93a66b0ce995118d33ff44f9be24cccffcee
Santiago Uria <uriasantiago.a@gmail.com>
2023-09-02 21:07:35 -0300

    deletes main method from view

--- src/view/Screen.java
+++ src/view/Screen.java
@@ -39,18 +39,6 @@ public class Screen extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Screen frame = new Screen();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	public static void switchTile(JButton buttons) {
 		if (buttons.getBackground().equals(_black)) {
