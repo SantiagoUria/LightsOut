07c9200ec7d991b22f35a58b4f13640d6e6d175d
Santiago Uria <uriasantiago.a@gmail.com>
2023-09-12 19:36:35 -0300

    moves App from model to App

--- src/view/App.java
+++ src/view/App.java
@@ -1,12 +1,9 @@
-package model;
-
-import view.Home;
+package view;
 
 public class App {
 	public static void main(String[] args) {
 		Home home = new Home();
 		home.setVisible(true);
 		home.setLocationRelativeTo(null);
-
 	}
 }
